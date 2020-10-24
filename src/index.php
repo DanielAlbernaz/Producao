35172be1285707d549af28c5d55a468d743f9658
DanielAlbernaz <54473406+DanielAlbernaz@users.noreply.github.com>
2020-10-24 18:13:12 -0300

    Criado o index

---
+++ src/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Olá Mundo!</title>
+</head>
+<body>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem sunt iure error necessitatibus nobis beatae, inventore atque dicta reprehenderit magni sint provident, veniam explicabo odit natus exercitationem cupiditate architecto modi!</p>
+</body>
+</html>
