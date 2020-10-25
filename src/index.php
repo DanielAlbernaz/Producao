a66dd2575689181bd1436b47af5ed6bd10e59a94
DanielAlbernaz <54473406+DanielAlbernaz@users.noreply.github.com>
2020-10-25 16:32:44 -0300

    conteudo escrito

--- src/index.php
+++ src/index.php
@@ -7,6 +7,7 @@
 </head>
 <body>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem sunt iure error necessitatibus nobis beatae, inventore atque dicta reprehenderit magni sint provident, veniam explicabo odit natus exercitationem cupiditate architecto modi!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos neque sint ipsa quibusdam? Beatae illo impedit sapiente! Repudiandae minima libero quasi illum rerum officiis ratione itaque qui laboriosam architecto. Corrupti..</p>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos neque sint ipsa quibusdam? Beatae illo impedit sapiente! Repudiandae minima libero quasi illum rerum officiis ratione itaque qui laboriosam architecto. Corrupti.</p>
 </body>
 </html>
